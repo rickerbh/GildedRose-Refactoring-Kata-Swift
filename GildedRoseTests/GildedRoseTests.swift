25add659ca61871fd65d5204b1ef307c3adf8fc0
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-22 08:30:27 +1000

    Add text based testing
    Add a 30 day run as a gold master test output.
    Add a test to compare the current code vs the original output to avoid regression.

--- GildedRoseTests/GildedRoseTests.swift
+++ GildedRoseTests/GildedRoseTests.swift
@@ -20,11 +20,40 @@ class GildedRoseTests: XCTestCase {
     super.tearDown()
   }
 
-  func testFoo() {
-    let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-    let app = GildedRose(items: items);
-    app.updateQuality();
-    XCTAssertEqual("fixme", app.items[0].name);
-  }
+  func testThirtyDaySales() {
+    let location = "/Users/rickerbh/Documents/Development/Apprenticeship/GildedRose/GildedRoseTests/"
+    let fileName = "out.gm"
+    let fileContents = try! String(contentsOfFile: location + fileName, encoding: String.Encoding.utf8)
+    let expectedValue = fileContents.components(separatedBy: CharacterSet.newlines)
 
+    let items = [
+      Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20), //
+      Item(name: "Aged Brie", sellIn: 2, quality: 0), //
+      Item(name: "Elixir of the Mongoosname: e", sellIn: 5, quality: 7), //
+      Item(name: "SulfursellIn: as,quality:  Hand of Ragnaros", sellIn: 0, quality: 80), //
+      Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+      Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+      Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+      Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+      // this conjured item does not work properly yet
+      Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
+
+    let app = GildedRose(items: items);
+    let days = 30
+
+    var runContent = [String]()
+
+    for i in 0..<days {
+      runContent.append("-------- day \(i) --------")
+      runContent.append("name, sellIn, quality")
+      for item in items {
+        runContent.append(item.description)
+      }
+      runContent.append("")
+      app.updateQuality()
+    }
+
+    XCTAssertEqual(expectedValue, runContent)
+
+  }
 }
