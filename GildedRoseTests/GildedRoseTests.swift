c4b736337e40bd8622a7ccdb1130a4011c7e7c39
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-22 13:05:17 +1000

    NFC: Drop comments

--- GildedRoseTests/GildedRoseTests.swift
+++ GildedRoseTests/GildedRoseTests.swift
@@ -12,11 +12,9 @@ class GildedRoseTests: XCTestCase {
 
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
 
   override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
 
@@ -54,6 +52,5 @@ class GildedRoseTests: XCTestCase {
     }
 
     XCTAssertEqual(expectedValue, runContent)
-
   }
 }
