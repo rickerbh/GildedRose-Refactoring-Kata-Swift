bbd3b0c71efec492cda6cc1326954098844cab9a
Hamish Rickerby <hamish@simplemachines.com.au>
2016-09-22 18:02:51 +1000

    Restructure if statements
    Just make them a bit easier to understand.
    Reindent - should have done this way earlier.

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -1,47 +1,47 @@
-
 public class GildedRose {
-    var items:[Item]
-    
-    required public init(items:[Item]) {
-        self.items = items
-    }
-    
-    public func updateQuality() {
-        
-        for i in 0..<items.count {
-          if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-              decrementQuality(items[i])
-            } else {
-                if (items[i].quality < 50) {
-                  incrementQuality(items[i])
+  var items:[Item]
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                          incrementQuality(items[i])
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                          incrementQuality(items[i])
-                        }
-                    }
-                }
+  required public init(items:[Item]) {
+    self.items = items
+  }
+
+  public func updateQuality() {
+
+    for i in 0..<items.count {
+      if (items[i].name == "Aged Brie" ||
+        items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (items[i].quality < 50) {
+          incrementQuality(items[i])
+
+          if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].sellIn < 11) {
+              incrementQuality(items[i])
             }
 
-          decrementSellDate(items[i])
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                      decrementQuality(items[i])
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                  incrementQuality(items[i])
-                }
+            if (items[i].sellIn < 6) {
+              incrementQuality(items[i])
             }
+          }
         }
+      } else {
+        decrementQuality(items[i])
+      }
+
+      decrementSellDate(items[i])
+
+      if (items[i].sellIn < 0) {
+        if items[i].name == "Aged Brie" {
+          incrementQuality(items[i])
+        } else {
+          if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            items[i].quality = items[i].quality - items[i].quality
+          } else {
+            decrementQuality(items[i])
+          }
+        }
+      }
     }
+  }
 
   internal func decrementQuality(_ item: Item) {
     guard item.quality > 0 else {
